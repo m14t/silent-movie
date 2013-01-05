0c7aea94285b45982c87399440d14e43264c556f
Matt Farmer <work@mattfarmer.net>
2013-01-04 21:32:39 -0800

    Use Silentina Film Font

--- web/display.php
+++ web/display.php
@@ -1,10 +1,21 @@
 <html>
     <head>
       <style>
+          @font-face {
+              font-family: 'silentina_filmregular';
+              src: url('font/typodermic_-_silentinafilm-regular-webfont.eot');
+              src: url('font/typodermic_-_silentinafilm-regular-webfont.eot?#iefix') format('embedded-opentype'),
+                   url('font/typodermic_-_silentinafilm-regular-webfont.woff') format('woff'),
+                   url('font/typodermic_-_silentinafilm-regular-webfont.ttf') format('truetype'),
+                   url('font/typodermic_-_silentinafilm-regular-webfont.svg#silentina_filmregular') format('svg');
+              font-weight: normal;
+              font-style: normal;
+
+          }
           body {
             color: #eee;
+            font-family: "silentina_filmregular", sans-serif;
             background:url(img/bg.png);
-            background-size:100% 100%; /* Firefox */
             background-size:100% 100%;
             background-repeat:no-repeat;
           }
@@ -30,7 +41,8 @@
             var socket = io.connect('http://<?php echo $_SERVER['HTTP_HOST']; ?>:8080'),
                 el = document.getElementById('output');
             socket.on('output', function (data) {
-                el.innerHTML = data;
+                //-- Wrap in Smart Quotes
+                el.innerHTML = '&ldquo;'+ data +'&rdquo;';
             });
         </script>
     </body>
