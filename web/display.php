b7abe86722ab05fbbda2674d69d8dc3657c2e2fc
Matt Farmer <work@mattfarmer.net>
2013-01-04 20:42:18 -0800

    Add background image and center text

--- web/display.php
+++ web/display.php
@@ -1,8 +1,29 @@
 <html>
     <head>
+      <style>
+          body {
+            color: #eee;
+            background:url(img/bg.png);
+            background-size:100% 100%; /* Firefox */
+            background-size:100% 100%;
+            background-repeat:no-repeat;
+          }
+          #wrapper {
+            display: table;
+            height: 100%;
+            width: 100%;
+          }
+          #output {
+            display: table-cell;
+            vertical-align: middle;
+            text-align: center;
+          }
+      </style>
     </head>
     <body>
-        <div id="output">
+        <div id="wrapper">
+          <div id="output">
+          </div>
         </div>
         <script src="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8080/socket.io/socket.io.js"></script>
         <script>
