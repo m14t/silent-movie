cc50edcaa368c61c09bd91cb74d22912d395970d
Matt Farmer <work@mattfarmer.net>
2013-02-02 20:03:08 -0800

    Save the last sent text

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,10 @@
-var io = require('socket.io').listen(8080);
+var io = require('socket.io').listen(8080),
+    last_msg = 'Connected...';
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('output', 'Connected...');
+    socket.emit('output', last_msg)
     socket.on('send', function (data) {
+        last_msg = data;
         socket.broadcast.emit('output', data);
     });
 });
